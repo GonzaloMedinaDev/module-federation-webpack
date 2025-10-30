fd19dee0a59add40e763fae759edb5978bfee532
Gonzalo Alfredo Medina Galdames <gonzalo.medina@tecnoavance.mx>
2025-10-30 11:01:35 -0700

    probando la libreria ux online

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ import {
 function App() {
   return (
     <ProviderCT>
-      <div className='max-w-[1080px] mx-auto bg-slate-300 p-4'>
+      <div className='bg-slate-300 p-6'>
         <HeadingCT type='primary'>App Remote (Webpack 5 / React 18)</HeadingCT>
-        <p className='mb-6 mt-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper enim in commodo pretium. Praesent aliquam, dolor eget sodales ultrices, elit nunc ullamcorper velit, ac mattis nulla dui at leo. In ac dolor a tortor consectetur scelerisque vel at nunc. In porttitor at nibh sit amet feugiat. Cras sit amet nulla id nisl blandit tempus.</p>
+        <p className='mb-6 mt-4'>{process.env.DEV_ENV} # Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper enim in commodo pretium. Praesent aliquam, dolor eget sodales ultrices, elit nunc ullamcorper velit, ac mattis nulla dui at leo. In ac dolor a tortor consectetur scelerisque vel at nunc. In porttitor at nibh sit amet feugiat. Cras sit amet nulla id nisl blandit tempus.</p>
 
         <CardCT title='Crear Póliza'>
           <FormCT>
